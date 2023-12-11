721bb20f9e62d27b811820c02777b515b0de0570
Heraldo <heraldofortuna@gmail.com>
2023-12-11 10:39:20 -0500

    definimos las páginas del proyecto y las creamos

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainPage from "./pages/Main/Main.vue";
+</script>
 
 <template>
-    <h1>Recargas y pagos</h1>
-    <span>Saldo virtual</span>
-    <span>S/ 150.00</span>
-    <p>¿Cómo cargar mi saldo virtual?</p>
-    <p>Código para cargar tu saldo: {12345678901}</p>
-    <div>
-        <img />
-        <span>Recargas</span>
-    </div>
-    <div>
-        <img />
-        <span>Pago de servicios</span>
-    </div>
-    <div>
-        <img />
-        <span>Historial de ganancias y operaciones</span>
-    </div>
+    <MainPage />
 </template>
+
+<style></style>
---
+++ src/pages/Main/Main.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+    <h1>Recargas y pagos</h1>
+    <span>Saldo virtual</span>
+    <span>S/ 150.00</span>
+    <p>¿Cómo cargar mi saldo virtual?</p>
+    <p>Código para cargar tu saldo: {12345678901}</p>
+    <div>
+        <img />
+        <span>Recargas</span>
+    </div>
+    <div>
+        <img />
+        <span>Pago de servicios</span>
+    </div>
+    <div>
+        <img />
+        <span>Historial de ganancias y operaciones</span>
+    </div>
+</template>
