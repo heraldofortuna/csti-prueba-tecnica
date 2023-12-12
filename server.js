848783596984c809f046b12790983c5f5b760366
Heraldo <heraldofortuna@gmail.com>
2023-12-12 07:20:18 -0500

    cambiamos la configuración ser servidor ssr

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 import express from "express";
+import { fileURLToPath } from "url";
 import { createServer as createViteServer } from "vite";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -9,60 +9,64 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 async function createServer() {
     const app = express();
 
-    // Create Vite server in middleware mode and configure the app type as
-    // 'custom', disabling Vite's own HTML serving logic so parent server
-    // can take control
+    // 1. Manejar solicitudes estáticas
+    app.use(express.static(path.resolve(__dirname, "dist/client")));
+
+    // 2. Obtener instancia de Vite
     const vite = await createViteServer({
         server: { middlewareMode: true },
         appType: "custom",
     });
 
-    // Use vite's connect instance as middleware. If you use your own
-    // express router (express.Router()), you should use router.use
-    // When the server restarts (for example after the user modifies
-    // vite.config.js), `vite.middlewares` is still going to be the same
-    // reference (with a new internal stack of Vite and plugin-injected
-    // middlewares. The following is valid even after restarts.
+    // 3. Utilizar el middleware de Vite
     app.use(vite.middlewares);
 
+    // 4. Manejar solicitudes de la aplicación
     app.use("*", async (req, res, next) => {
         const url = req.originalUrl;
 
         try {
-            // 1. Read index.html
+            // 5. Leer index.html
             let template = fs.readFileSync(
                 path.resolve(__dirname, "index.html"),
                 "utf-8"
             );
 
-            // 2. Apply Vite HTML transforms. This injects the Vite HMR client,
-            //    and also applies HTML transforms from Vite plugins, e.g. global
-            //    preambles from @vitejs/plugin-react
+            // 6. Aplicar transformaciones HTML de Vite
             template = await vite.transformIndexHtml(url, template);
 
-            // 3. Load the server entry. ssrLoadModule automatically transforms
-            //    ESM source code to be usable in Node.js! There is no bundling
-            //    required, and provides efficient invalidation similar to HMR.
+            // 7. Cargar la entrada del servidor
             const { render } = await vite.ssrLoadModule("/src/entry-server.ts");
 
-            // 4. render the app HTML. This assumes entry-server.ts's exported
-            //     `render` function calls appropriate framework SSR APIs,
-            //    e.g. ReactDOMServer.renderToString()
+            // 8. Renderizar la aplicación HTML
             const appHtml = await render(url);
 
-            // 5. Inject the app-rendered HTML into the template.
+            // 9. Inyectar el HTML renderizado de la aplicación en la plantilla
             const html = template.replace(`<!--ssr-outlet-->`, appHtml);
 
-            // 6. Send the rendered HTML back.
+            // 10. Enviar el HTML renderizado
             res.status(200).set({ "Content-Type": "text/html" }).end(html);
         } catch (e) {
-            // If an error is caught, let Vite fix the stack trace so it maps back
-            // to your actual source code.
+            // 11. Manejar errores
             vite.ssrFixStacktrace(e);
             next(e);
         }
     });
 
+    // 12. Manejar otras rutas aquí según tus necesidades
+    app.get("/recharges", (req, res) => {
+        console.log("recharges:", req, res);
+        res.send("Hola desde /recharges!");
+    });
+
+    // 13. Manejar errores
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+
+        res.status(500).send("Something went wrong!");
+    });
+
+    // 14. Iniciar el servidor
     app.listen(3000);
 }
 
