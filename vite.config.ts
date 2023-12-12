2ee7ca8f1d74bd23a89609a4d5e57473b07e0115
Heraldo <heraldofortuna@gmail.com>
2023-12-12 10:55:25 -0500

    se añadió la config de pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [Vue()],
+    plugins: [
+        Vue(),
+        VitePWA({
+            injectRegister: "auto",
+            registerType: "autoUpdate",
+            workbox: {
+                clientsClaim: true,
+                skipWaiting: true,
+            },
+        }),
+    ],
 });
