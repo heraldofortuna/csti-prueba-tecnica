1f6e605efbb7cf300c2b07ddd6ff4e6008434b70
Heraldo <heraldofortuna@gmail.com>
2023-12-12 11:06:21 -0500

    se añadió el manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,14 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
+import manifest from "./manifest.json";
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         Vue(),
         VitePWA({
-            injectRegister: "auto",
-            registerType: "autoUpdate",
-            workbox: {
-                clientsClaim: true,
-                skipWaiting: true,
-            },
+            manifest: manifest,
         }),
     ],
 });
