4d7d19dd75749f78d48f17df244fcce150ff2545
Heraldo <heraldofortuna@gmail.com>
2023-12-12 11:17:49 -0500

    se modificó el manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,28 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
-import manifest from "./manifest.json";
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         Vue(),
         VitePWA({
-            manifest: manifest,
+            manifest: {
+                name: "Culqi PWA Test",
+                short_name: "Culqi",
+                description:
+                    "Una aplicación web progresiva construida con Vite y Vue para Culqi",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#000000",
+                icons: [
+                    {
+                        src: "/icon.png",
+                        sizes: "48x48 72x72 96x96 144x144 192x192 256x256 384x384 512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                ],
+            },
         }),
     ],
 });
